5ad864d44b4e365f66f9137f5ef95b44ec99f1d7
Noah Betteridge <nonobett4@gmail.com>
2025-04-15 11:36:30 +1200

    New Block And Ore Updates

--- src/main/java/com/nonobett/oresrevamped/block/ModBlocks.java
+++ src/main/java/com/nonobett/oresrevamped/block/ModBlocks.java
@@ -4,7 +4,9 @@ import com.nonobett.oresrevamped.OresRevamped;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.block.MapColor;
+import net.minecraft.block.enums.NoteBlockInstrument;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -12,10 +14,20 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block RUBY_BLOCK = registerBlock("ruby_block",
             new Block(AbstractBlock.Settings.create().mapColor(MapColor.RED).requiresTool().strength(50.0F).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+     public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
+             new Block(AbstractBlock.Settings.create().mapColor(MapColor.RED).requiresTool().strength(45.0F).instrument(NoteBlockInstrument.BASS)));
+
+    public static final Block RUBY_ORE = registerBlock("ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 7), AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).requiresTool().strength(3.0F).instrument(NoteBlockInstrument.BASEDRUM)));
+
+    public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 7), AbstractBlock.Settings.create().mapColor(MapColor.DEEPSLATE_GRAY).requiresTool().strength(4.5F).sounds(BlockSoundGroup.DEEPSLATE)));
 
 
     private static Block registerBlock(String name, Block block) {
@@ -33,6 +45,12 @@ public class ModBlocks {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
             entries.add(ModBlocks.RUBY_BLOCK);
+            entries.add(ModBlocks.RAW_RUBY_BLOCK);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
+            entries.add(ModBlocks.RUBY_ORE);
+            entries.add(ModBlocks.DEEPSLATE_RUBY_ORE);
         });
     }
 }
