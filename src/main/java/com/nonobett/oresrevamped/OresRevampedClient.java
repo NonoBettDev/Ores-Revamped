d6ae6a81d3721397fa5c2b1aef452c9900ecaa85
Noah Betteridge <nonobett4@gmail.com>
2025-04-14 15:00:21 +1200

    Initial commit

---
+++ src/main/java/com/nonobett/oresrevamped/OresRevampedClient.java
@@ -0,0 +1,10 @@
+package com.nonobett.oresrevamped;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OresRevampedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/nonobett/oresrevamped/OresRevampedDataGenerator.java
@@ -0,0 +1,11 @@
+package com.nonobett.oresrevamped;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OresRevampedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
